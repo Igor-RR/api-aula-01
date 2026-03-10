2c595ff22216f42ec239b1f6513fe48b625acb56
Ígor Rueda <igordriguesrueda15@gmail.com>
2026-03-10 01:42:58 +0000

    Adicionando GET, POST, PUT e DELETE

--- src/server.ts
+++ src/server.ts
@@ -4,11 +4,8 @@ import { request } from "node:http";
 const app = express();
 const PORT = 3000;
 
+/* Configura um middleware do express para converter objetos javascript em json*/
 app.use(express.json());
-
-app.listen(PORT,() => {
-    console.log("Servidor executando em localhost");
-});
 
 app.get("/",(req: Request, res: Response) => {
     res.send("Olá express!!!"); 
@@ -16,6 +13,21 @@ app.get("/",(req: Request, res: Response) => {
 
 app.get("/saudacao/:nome",(req: Request, res: Response) => {
     const nome = req.params.nome;
-    res.send("Olá " + nome);
+    /*res.send("Olá " + nome);*/
+    res.json({
+        mensagem: `Olá ${nome}!`
+    });
 
-})
+});
+
+app.post("/saudacao",(req: Request, res: Response)=>{
+    const {nome} = req.body;
+
+    res.json({
+        mensagem:`Olá ${nome}`!
+    })
+});
+
+app.listen(PORT,() => {
+    console.log("Servidor executando em localhost");
+});
